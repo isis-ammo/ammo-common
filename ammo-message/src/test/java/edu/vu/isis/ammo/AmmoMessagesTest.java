10477376094e7c6a417af63be46f0523eb0d6e59
Fred Eisele <phreed@gmail.com>
2012-11-02 12:09:24 -0500

    build using the 2.4.1 version of protoc

---
+++ ammo-message/src/test/java/edu/vu/isis/ammo/AmmoMessagesTest.java
@@ -0,0 +1,19 @@
+package edu.vu.isis.ammo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AmmoMessagesTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
